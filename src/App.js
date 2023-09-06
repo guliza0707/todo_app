b3566b03da7e02c7233644fe1849b226ebdf1c39
guliztulkinova <guliztulkinova@gmail.com>
2023-09-06 13:55:18 +0500

    created version 2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>
-        Hello world!
-      </h1>
-    </div>
+      <h1>Hello world!</h1>
+        <p>version 2</p>
+      </div>
     
   );
 }
